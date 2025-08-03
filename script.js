3f93a9faa8ce8ef99a002455e40cf8b6982028fe
PETER MUSSA <nassoropeter97@gmail.com>
2025-08-03 19:05:28 +0300

    add changes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Create remove button
         const removeBtn = document.createElement('button');
         removeBtn.textContent = "Remove";
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn'); // Use classList.add to add the class
 
         // When remove button is clicked, remove the list item
         removeBtn.onclick = function () {
